0c14817805e77130f627328441e530084815bf01
samim1097 <waysamim@gmail.com>
2020-05-13 16:00:14 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ import Person from './Person'
       <Person name ={this.state.person[0].name} age= {this.state.person[0].age}/>
       <Person name ={this.state.person[1].name} age= {this.state.person[1].age}/>
       <Person name ={this.state.person[2].name} age= {this.state.person[2].age}/>
+      <Person name ={this.state.person[2].name} age= {this.state.person[2].age}/>
+      
       <button>click me </button>
     </div>
   );
